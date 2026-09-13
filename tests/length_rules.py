f63b6e3498a47cea8ef22e18c75194a96ea3c456
pragyansingh1010 <pragyansingh1010@gmail.com>
2026-09-13 14:33:55 +0530

    test: validate password length rules

---
+++ tests/length_rules.py
@@ -0,0 +1,8 @@
+def valid_length(n):
+    return isinstance(n, int) and n >= 1
+
+assert valid_length(1)
+assert valid_length(12)
+assert not valid_length(0)
+assert not valid_length(-4)
+print('Password length rules passed')
